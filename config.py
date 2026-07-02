92c33ab2059448ea714d1e247ca35f71b9be940a
Sharini <sharini1012@gmail.com>
2026-07-02 13:47:52 +0530

    Prepare project for deployment

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = "academicassistant123"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "academicassistant123")
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root123@localhost/academic_assistant"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "mysql+pymysql://root:root123@localhost/academic_assistant"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
