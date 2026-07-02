b6f84fd45705fc0000a5c6c2bcfa8f2840aa41b3
Sharini <sharini1012@gmail.com>
2026-07-02 13:55:26 +0530

    Use environment variables

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "academicassistant123")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "academicassistant123"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://root:root123@localhost/academic_assistant"
+        "sqlite:///academic.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
