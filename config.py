4900b3e262858a4125ad92e0a8887b7cf4082fc7
Sharini <sharini1012@gmail.com>
2026-07-02 15:09:30 +0530

    Prepare Docker deployment

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 import os
 
 class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "academicassistant123")
 
-    SECRET_KEY = os.getenv(
-        "SECRET_KEY",
-        "academicassistant123"
-    )
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
+    database_url = os.getenv(
         "DATABASE_URL",
-        "sqlite:///academic.db"
+        "mysql+pymysql://root:root123@localhost/academic_assistant"
     )
+
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql+psycopg2://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
